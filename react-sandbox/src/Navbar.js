78994bf53edd8b6c2d79ff49bcb4d5a626724cd4
olaydub <olaydubproject@gmail.com>
2022-10-23 16:10:00 -0700

    new css

---
+++ react-sandbox/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Control Cognizant</h1>
+            <div className="links">
+                <a href="https://nist.gov">NIST</a>
+                <a href="https://hitrustalliance.net/">HITRUST</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
