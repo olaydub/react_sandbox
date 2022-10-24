2f04a11b3ae8557ff8ab73287a827d177dc6a2f1
olaydub <olaydubproject@gmail.com>
2022-10-23 18:12:28 -0700

    useState to display which view is chosen in dashboard

--- react-sandbox/src/Home.js
+++ react-sandbox/src/Home.js
@@ -1,16 +1,20 @@
-
+import { useState } from 'react';
 
 const Home = () => {
+    //let view = 'default dash view'
+    const [view, setView] = useState('default view')
 
     const handleClick = (name, e) => {
+        setView(name + ' chosen');
         console.log('clicked: ' + name, e.target);
     }
 
     return (
         <div className = "Home">
             <h2>Dashboard</h2>
-            <button onClick={(e) => handleClick('view', e)}>View Controls</button>
-            <button onClick={(e) => handleClick('update', e)}>Update Controls</button>
+            <p> { view } </p>
+            <button onClick={(e) => handleClick('view controls', e)}>View Controls</button>
+            <button onClick={(e) => handleClick('update controls', e)}>Update Controls</button>
         </div>
     );
 }
